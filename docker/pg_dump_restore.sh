c09f1ef97728f4554dd6d28be9152fd6311287bf
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-06-14 23:24:03 +0300

    RANCHER-313

--- docker/pg_dump_restore.sh
+++ docker/pg_dump_restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DUMP_FILE=/mnt/ebs-volume/backup_`date +%Y%m%d_%H%M%S`.pgdump
-PGPASSWORD=$POSTGRES_PASSWORD pg_dump -Fc -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -Fc -d $PGDATABASE -U $PGUSER -h $PGHOST-f $DUMP_FILE
 echo "pg_dump operation SUCCESSFULLY COMPLETED"
 aws s3 cp ${DUMP_FILE} $S3_BACKUP_PATH/$RANCHER_CLUSTER_PROJECT_NAME/
 echo "AWS s3 cp operation SUCCESSFULLY COMPLETED"
