79746951b049a45c61cc118a93c51d9d00f894a1
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-06-22 12:17:09 +0300

    RANCHER-313

--- docker/pg_dump_restore.sh
+++ docker/pg_dump_restore.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-set -e
-
-export DUMP_FILE=/mnt/ebs-volume/backup_`date +%Y-%m-%d_%H:%M:%S`.pgdump
-{
-PGPASSWORD=$DB_PASSWORD pg_dump -Fc -d $DB_DATABASE -U $DB_USERNAME -h $DB_HOST -f $DUMP_FILE
-echo "pg_dump operation SUCCESSFULLY COMPLETED"
-aws s3 cp ${DUMP_FILE} $S3_BACKUP_PATH/$RANCHER_CLUSTER_PROJECT_NAME/
-echo "AWS s3 cp operation SUCCESSFULLY COMPLETED"
-} ||
-{
+(
+  set -e
+  export DUMP_FILE=/mnt/ebs-volume/backup_`date +%Y-%m-%d_%H:%M:%S`.pgdump
+  PGPASSWORD=$DB_PASSWORD pg_dump -Fc -d $DB_DATABASE -U $DB_USERNAME -h $DB_HOST -f $DUMP_FILE
+  echo "pg_dump operation SUCCESSFULLY COMPLETED"
+  aws s3 cp ${DUMP_FILE} $S3_BACKUP_PATH/$RANCHER_CLUSTER_PROJECT_NAME/
+  echo "AWS s3 cp operation SUCCESSFULLY COMPLETED"
+)
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
   echo "pg_dump operation FAILED (postgres backup aws s3 cp failed)"
-  exit 1
-}
-
+  exit $errorCode
+fi
