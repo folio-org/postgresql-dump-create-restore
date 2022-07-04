0d19b549cb1a51e3e3eb8da0d5a5d31b8b3585cb
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-07-04 11:56:08 +0300

    RANCHER-313

--- docker/pg_dump_restore.sh
+++ docker/pg_dump_restore.sh
@@ -2,7 +2,7 @@
 
 (
   set -e
-  export DUMP_FILE=/mnt/ebs-volume/backup_${JENKINS_BUILD_ID}-${JENKINS_START_BUILD_DATE_TIME}-${JENKINS_START_BUILD_USERNAME}.pgdump
+  export DUMP_FILE=/mnt/ebs-volume/${JENKINS_DB_BACKUP_NAME}.pgdump
   PGPASSWORD=$DB_PASSWORD pg_dump -Fc -d $DB_DATABASE -U $DB_USERNAME -h $DB_HOST -f $DUMP_FILE
   echo "pg_dump operation SUCCESSFULLY COMPLETED. Path to file is $DUMP_FILE"
   aws s3 cp ${DUMP_FILE} $AWS_BUCKET/$RANCHER_CLUSTER_PROJECT_NAME/
