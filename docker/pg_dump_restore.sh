bd6c0b802cb6b051b5383b7a97f5c828a3cfa924
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-10-31 17:11:51 +0200

    RANCHER-503

--- docker/pg_dump_restore.sh
+++ docker/pg_dump_restore.sh
@@ -12,14 +12,13 @@ if [ "$ACTION" == backup ]; then
     echo "SUCCESSFULLY got okapiToken for tenant - $TENANT, adminUser - $TENANT_ADMIN_USERNAME, environment - https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/authn/login"
     installedModules=$(curl -X GET https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/_/proxy/tenants/${TENANT}/modules  -H "Content-Type: application/json" -H "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${okapiToken}")
     echo "SUCCESSFULLY got list of installedModules for tenant - $TENANT, adminUser - $TENANT_ADMIN_USERNAME, environment - https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/authn/login"
-    jq '.[] | . += { "action": "enable" }' <<<"$installedModules" | jq '.' -s > "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}_install.json"
-    aws s3 cp "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}_install.json" "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/"
-    echo "AWS s3 cp operation of file ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}_install.json to s3 ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/ bucket SUCCESSFULLY COMPLETED"
+    jq '.[] | . += { "action": "enable" }' <<<"$installedModules" | jq '.' -s > "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-install.json"
+    aws s3 cp "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-install.json" "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/"
+    echo "AWS s3 cp operation of file ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-install.json to s3 ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/ bucket SUCCESSFULLY COMPLETED"
   )
   errorCode=$?
   if [ $errorCode -ne 0 ]; then
     echo "pg_dump operation FAILED (postgres backup aws s3 cp failed)"
-    sleep 60
     exit $errorCode
   fi
 elif [ "$ACTION" == restore ]; then
@@ -33,7 +32,6 @@ elif [ "$ACTION" == restore ]; then
     errorCode=$?
     if [ $errorCode -ne 0 ]; then
       echo "psql restore operation FAILED (postgres restore aws s3 cp failed)"
-      sleep 60
       exit $errorCode
     fi
 fi
