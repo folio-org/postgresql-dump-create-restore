48db394c7c1e3cc298d8ab68a02da9c4bcc583b7
Mykhailo_Teliatnikov <Mykhailo_Teliatnikov@epam.com>
2023-03-03 16:46:51 +0200

    Add new action and variables

--- docker/pg_dump_restore.sh
+++ docker/pg_dump_restore.sh
@@ -26,7 +26,7 @@ elif [ "$ACTION" == restore ]; then
     set -e
     aws s3 cp "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}.psql" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
     echo "AWS s3 cp operation of file ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}.psql to ${EBS_VOLUME_MOUNT_PATH} path SUCCESSFULLY COMPLETED"
-    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT < "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql" > /dev/null
+    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT < "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql" >> "${EBS_VOLUME_MOUNT_PATH}/import.log"
     echo "psql restore operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
     )
     errorCode=$?
@@ -34,5 +34,54 @@ elif [ "$ACTION" == restore ]; then
       echo "psql restore operation FAILED (postgres restore aws s3 cp failed)"
       exit $errorCode
     fi
+elif [ "$ACTION" == rds_backup ]; then
+  (
+    set -e
+    echo "Start backup"
+
+    PGPASSWORD=${TEMPORARY_RDS_PASSWORD} pg_dumpall --globals-only --no-role-passwords \
+    -h ${TEMPORARY_RDS_HOST} -U ${TEMPORARY_RDS_USERNAME} -p ${TEMPORARY_RDS_PORT} -l ${TEMPORARY_RDS_DATABASE} \
+    -f "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-globals.psql"
+
+    PGPASSWORD=${TEMPORARY_RDS_PASSWORD} pg_dump -Z0 -j 5 -Fd  \
+    -h ${TEMPORARY_RDS_HOST} -U ${TEMPORARY_RDS_USERNAME} -p ${TEMPORARY_RDS_PORT} -d ${TEMPORARY_RDS_DATABASE} \
+    -f "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-dir"
+
+    tar -cf "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.tar" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-dir"
+
+    echo "pg_dumpall operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
+    aws s3 cp "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-globals.psql" "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/"
+    aws s3 cp "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.tar" "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/"
+    echo "AWS s3 cp operation of file ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql to s3 ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/ bucket SUCCESSFULLY COMPLETED"
+    )
+  errorCode=$?
+  if [ $errorCode -ne 0 ]; then
+    echo "pg_dump operation FAILED (postgres backup aws s3 cp failed)"
+    exit $errorCode
+  fi
+elif [ "$ACTION" == rds_restore ]; then
+  (
+    set -e
+    echo "Start backup"
+
+    aws s3 cp "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}-globals.psql" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-globals.psql"
+    aws s3 cp "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}.tar" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.tar"
+    echo "AWS s3 cp operation of file ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}.psql to ${EBS_VOLUME_MOUNT_PATH} path SUCCESSFULLY COMPLETED"
+
+    tar -xvf "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.tar"
+
+    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT < "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-globals.psql" >> "${EBS_VOLUME_MOUNT_PATH}/globals-import.log"
+    #schema-only
+    PGPASSWORD=$DB_PASSWORD pg_restore -j 5 -Fd -vvv -h $DB_HOST -U $DB_USERNAME -p $DB_PORT -d "${TEMPORARY_RDS_DATABASE}" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-dir" >> "${EBS_VOLUME_MOUNT_PATH}/import.log"
+    #data-only
+    PGPASSWORD=$DB_PASSWORD pg_restore -j 5 -Fd -vvv -a -h $DB_HOST -U $DB_USERNAME -p $DB_PORT -d "${TEMPORARY_RDS_DATABASE}" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-dir" >> "${EBS_VOLUME_MOUNT_PATH}/data-import.log"
+    echo "psql restore operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
+
+    )
+  errorCode=$?
+  if [ $errorCode -ne 0 ]; then
+    echo "pg_dump operation FAILED (postgres backup aws s3 cp failed)"
+    exit $errorCode
+  fi
 fi
 
