3de45239edda13e621b71d10c8180ad50396ebaa
eldiiar-duishenaliev <Eldiiar_Duishenaliev@epam.com>
2024-12-12 15:43:13 +0600

    RANCHER-1943 updated files

--- docker/pg_dump_restore.sh
+++ docker/pg_dump_restore.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
-
-if [ "$ACTION" == backup ]; then
-  (
-    set -e
-    PGPASSWORD=${DB_PASSWORD} pg_dumpall -h ${DB_HOST} -U ${DB_USERNAME} -p ${DB_PORT} -l ${DB_DATABASE} \
-    -f "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
-    echo "pg_dumpall operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
-    aws s3 cp "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql" "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/"
-    echo "AWS s3 cp operation of file ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql to s3 ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/ bucket SUCCESSFULLY COMPLETED"
-    okapiToken=$(curl -X POST https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/authn/login -H "Content-Type: application/json" -H "X-Okapi-Tenant: ${TENANT}" -d '{"username": "'"${TENANT_ADMIN_USERNAME}"'", "password": "'"${TENANT_ADMIN_PASSWORD}"'"}' | jq '.okapiToken' -r)
-    echo "SUCCESSFULLY got okapiToken for tenant - $TENANT, adminUser - $TENANT_ADMIN_USERNAME, environment - https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/authn/login"
-    installedModules=$(curl -X GET https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/_/proxy/tenants/${TENANT}/modules  -H "Content-Type: application/json" -H "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${okapiToken}")
-    echo "SUCCESSFULLY got list of installedModules for tenant - $TENANT, adminUser - $TENANT_ADMIN_USERNAME, environment - https://${RANCHER_CLUSTER_NAME}-${RANCHER_PROJECT_NAME}-okapi.ci.folio.org/authn/login"
-    jq '.[] | . += { "action": "enable" }' <<<"$installedModules" | jq '.' -s > "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-install.json"
-    aws s3 cp "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-install.json" "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/"
-    echo "AWS s3 cp operation of file ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}-install.json to s3 ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/ bucket SUCCESSFULLY COMPLETED"
-  )
-  errorCode=$?
-  if [ $errorCode -ne 0 ]; then
-    echo "pg_dump operation FAILED (postgres backup aws s3 cp failed)"
-    exit $errorCode
-  fi
-elif [ "$ACTION" == restore ]; then
-  (
-    set -e
-    aws s3 cp "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}.psql" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
-    echo "AWS s3 cp operation of file ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}/${DB_BACKUP_NAME}.psql to ${EBS_VOLUME_MOUNT_PATH} path SUCCESSFULLY COMPLETED"
-    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT < "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql" > /dev/null
-    echo "psql restore operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.psql"
-    )
-    errorCode=$?
-    if [ $errorCode -ne 0 ]; then
-      echo "psql restore operation FAILED (postgres restore aws s3 cp failed)"
-      exit $errorCode
-    fi
-fi
-
+set -e
+echo "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}.sql"
+echo "${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_DATA}.tar"
+echo "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.sql"
+echo "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_DATA}.tar"
+echo "$(pwd)"
+aws --region us-west-2 s3 cp "s3://${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}.sql" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.sql"
+echo "AWS s3 cp operation of file ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_NAME}.sql to ${EBS_VOLUME_MOUNT_PATH} path SUCCESSFULLY COMPLETED"
+aws --region us-west-2 s3 cp "s3://${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_DATA}.tar" "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_DATA}.tar"
+echo "AWS s3 cp operation of file ${S3_BACKUPS_BUCKET}/${S3_BACKUPS_DIRECTORY}/${DB_BACKUP_DATA}.tar to ${EBS_VOLUME_MOUNT_PATH} path SUCCESSFULLY COMPLETED"
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -p $DB_PORT --dbname="folio" -a -f "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.sql"
+echo "psql restore operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_NAME}.sql"
+PGPASSWORD=$DB_PASSWORD pg_restore -h $DB_HOST -U $DB_USERNAME -p $DB_PORT --dbname="folio" --verbose --format="tar" --exit-on-error "${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_DATA}.tar"
+echo "pg_restore operation SUCCESSFULLY COMPLETED. Path to file is ${EBS_VOLUME_MOUNT_PATH}/${DB_BACKUP_DATA}.tar"
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+  echo "psql restore operation FAILED (postgres restore aws s3 cp failed)"
+  exit $errorCode
